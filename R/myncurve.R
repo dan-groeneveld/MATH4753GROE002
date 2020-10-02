10ccd75a4c4e7f587eb26efe8ea05b8623d9c125
Dan Groeneveld <dcgroeneveld@ou.edu>
2020-10-02 18:14:07 -0500

    Lab 6 Submitted!

---
+++ R/myncurve.R
@@ -0,0 +1,30 @@
+#' myncurve
+#'
+#' @param mu the mean
+#' @param sigma the standard deviation
+#' @param a the value to which you want to calculate the probability from negative infinity
+#'
+#' @return returns a plot with shaded area and prints out the probability
+#' @export
+#'
+#' @examples
+myncurve = function(mu, sigma, a){
+  curve(dnorm(x,mean=mu,sd=sigma), xlim = c(mu-3*sigma, mu + 3*sigma))
+
+  # x values corresponding to the x - cords of points on the curve
+  xcurve=seq(mu-3*sigma,a,length=1000)
+
+  # Y values corresponding t0 the x values
+  ycurve=dnorm(xcurve,mean=mu,sd=sigma)
+
+  # Fill in the polygon with the given vertices
+  polygon(c(mu-3*sigma,xcurve,a),c(0,ycurve,0),col="Red")
+
+  # Put in the text with the appropriate area
+
+  # Area
+  prob=pnorm(a,mean=mu,sd=sigma)
+  prob=round(prob,4)
+  prob
+
+}
